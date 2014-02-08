bf76e557bdbeb4d220ea77a320de60b78297696d
Michael Ahlers <michael@ahlers.co>
2014-02-08 11:58:18 -0500

    Baseline.

--- build.sbt
+++ build.sbt
@@ -4,8 +4,11 @@ version := "1.0-SNAPSHOT"
 
 libraryDependencies ++= Seq(
   "org.reactivemongo" %% "play2-reactivemongo" % "0.10.2",
+  "org.webjars" %% "webjars-play" % "2.2.1",
   "org.webjars" % "angularjs" % "1.2.11",
-  "org.webjars" % "bootstrap" % "3.1.0"
+  "org.webjars" % "bootstrap" % "3.1.0",
+  "org.webjars" % "jquery" % "2.1.0-1",
+  "org.webjars" % "requirejs" % "2.1.10"
 )
 
 play.Project.playScalaSettings
--- public/index.js
+++ public/index.js
@@ -1,109 +1,112 @@
 'use strict'
 
 require({
-  waitSeconds: 60,
-
-  map: {
-    '*': {
-      /* RequireJS plugins. */
-      'async': 'vendor/requirejs-plugins/src/async',
-      'json': 'vendor/requirejs-plugins-master/src/json',
-      'goog': 'vendor/requirejs-plugins-master/src/goog',
-      'noext': 'vendor/requirejs-plugins-master/src/noext',
-      'text': 'vendor/requirejs-text/text'
-    }
-  },
-
+//  waitSeconds: 60,
+//
+//  map: {
+//    '*': {
+//      /* RequireJS plugins. */
+//      'async': 'vendor/requirejs-plugins/src/async',
+//      'json': 'vendor/requirejs-plugins-master/src/json',
+//      'goog': 'vendor/requirejs-plugins-master/src/goog',
+//      'noext': 'vendor/requirejs-plugins-master/src/noext',
+//      'text': 'vendor/requirejs-text/text'
+//    }
+//  },
+//
   paths: {
     /* Core AngularJS packages. */
-    'angular': 'vendor/angular/angular',
-    'angular-animate': 'vendor/angular-animate/angular-animate',
-    'angular-resource': 'vendor/angular-resource/angular-resource',
-    'angular-route': 'vendor/angular-route/angular-route',
-    'angular-sanitize': 'vendor/angular-sanitize/angular-sanitize',
-
-    'bootstrap': 'vendor/bootstrap/dist/js/bootstrap',
-
-    'jquery': 'vendor/jquery/jquery',
-
-    'moment': 'vendor/moment/moment',
-
-    'restangular': 'vendor/restangular/dist/restangular',
-
-    'underscore': 'vendor/underscore/underscore'
+//    'angular': 'vendor/angular/angular',
+//    'angular-animate': 'vendor/angular-animate/angular-animate',
+//    'angular-resource': 'vendor/angular-resource/angular-resource',
+//    'angular-route': 'vendor/angular-route/angular-route',
+//    'angular-sanitize': 'vendor/angular-sanitize/angular-sanitize',
+//
+//    'bootstrap': 'vendor/bootstrap/dist/js/bootstrap',
+//
+    'jquery': 'webjars/jquery/2.1.0/jquery.min'
+//
+//    'moment': 'vendor/moment/moment',
+//
+//    'restangular': 'vendor/restangular/dist/restangular',
+//
+//    'underscore': 'vendor/underscore/underscore'
   },
-
+//
   shim: {
-    'angular': {
-      deps: [ 'jquery' ],
-      exports: 'angular'
-    },
-
-    'angular-animate': ['angular'],
-    'angular-resource': ['angular'],
-    'angular-route': ['angular'],
-    'angular-sanitize': ['angular'],
-
-    'bootstrap': ['jquery'],
-
+//    'angular': {
+//      deps: [ 'jquery' ],
+//      exports: 'angular'
+//    },
+//
+//    'angular-animate': ['angular'],
+//    'angular-resource': ['angular'],
+//    'angular-route': ['angular'],
+//    'angular-sanitize': ['angular'],
+//
+//    'bootstrap': ['jquery'],
+//
     'jquery': {
       exports: 'jQuery'
-    },
-
-    'restangular': {
-      deps: [ 'angular', 'underscore' ]
-    },
-
-    'underscore': {
-      exports: '_'
     }
-  },
-
-  optimize: 'uglify2',
-  uglify2: {
-    warnings: false,
-    /* Mangling defeats Angular injection by function argument names. */
-    mangle: false
+//
+//    'restangular': {
+//      deps: [ 'angular', 'underscore' ]
+//    },
+//
+//    'underscore': {
+//      exports: '_'
+//    }
   }
+//
+//  optimize: 'uglify2',
+//  uglify2: {
+//    warnings: false,
+//    /* Mangling defeats Angular injection by function argument names. */
+//    mangle: false
+//  }
 })
 
-define([
+require([
   'jquery'
-  , 'angular'
+//  , 'angular'
+//
+//  , 'angular-route'
+//  , 'restangular'
+], function ($) {
 
-  , 'angular-route'
-  , 'restangular'
-], function ($, angular) {
+
 
   $(function () {
-
-    angular
-      .module('taw', [
-        'ngRoute',
-        'restangular'
-      ])
-
-      .config(function ($routeProvider) {
-        $routeProvider.otherwise({
-          redirectTo: '/'
-        })
-      })
-
-      .run(function (Restangular) {
-        Restangular.setBaseUrl('/services')
-
-        Restangular.setRestangularFields({
-          id: "_id.$oid"
-        })
-      })
-
-    /* The requirements on this script ensure the application module has been
-     * defined prior to bootstrapping AngularJS. If this is done in the wrong
-     * order, there will be visible flicker as ngCloak is removed and the main
-     * application scope is digested. */
-
-    angular.bootstrap(document, ['taw'])
-
+    console.log('Loaded!')
+//
+//    angular
+//      .module('taw', [
+//        'ngRoute',
+//        'restangular'
+//      ])
+//
+//      .config(function ($routeProvider) {
+//        $routeProvider.otherwise({
+//          redirectTo: '/'
+//        })
+//      })
+//
+//      .run(function (Restangular) {
+//        Restangular.setBaseUrl('/services')
+//
+//        Restangular.setRestangularFields({
+//          id: "_id.$oid"
+//        })
+//      })
+//
+//    /* The requirements on this script ensure the application module has been
+//     * defined prior to bootstrapping AngularJS. If this is done in the wrong
+//     * order, there will be visible flicker as ngCloak is removed and the main
+//     * application scope is digested. */
+//
+//    angular.bootstrap(document, ['taw'])
+//
   })
 })
 
