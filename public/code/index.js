c8ddebd685ca74a054cdc4efb1e6a829f76f48f4
Michael Ahlers <michael@ahlers.co>
2014-02-16 00:24:20 -0500

    Login workflow.

--- public/code/index.js
+++ public/code/index.js
@@ -4,18 +4,36 @@ define([
 
   , 'text!./template.html'
 
+  , 'angular-resource'
   , 'restangular'
 ], function ($, angular, template) {
   return angular
-    .module('taw.code', [ 'restangular' ])
+    .module('taw.code', [ 'ngResource', 'restangular' ])
 
     .directive('tawCode', function () {
       return {
         restrict: 'A',
         template: template,
         replace: true,
-        scope: true,
-        link: function (scope, iEl, Attrs, ctrl) {
+        scope: {
+          'onEnter': '&'
+        },
+        controller: function ($scope, $resource) {
+          $scope.submit = function () {
+            $scope.loading = true
+            $resource('/enter/:code').get({
+              code: $scope.code
+            }, function (group) {
+              $scope.onEnter({
+                group: group
+              })
+              $scope.error = null
+              $scope.loading = false
+            }, function () {
+              $scope.error = 'Wrong code. Try again?'
+              $scope.loading = false
+            })
+          }
         }
       }
     })
