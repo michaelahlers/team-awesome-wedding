6c27b619297fe1b1a0d354cb0f10f8fcf96220e4
Michael Ahlers <michael@ahlers.co>
2014-02-16 00:09:23 -0500

    Resolve and prepopulate the group if the user's already been here.

--- public/controller.js
+++ public/controller.js
@@ -1,7 +1,18 @@
 'use strict'
 
 define(['angular', 'underscore'], function (angular, _) {
-  return function ($rootScope, $scope) {
-
+  var controller = function ($rootScope, $scope, $log, group) {
+    $scope.group = group
   }
+
+  controller.resolve = {
+    group: function ($rootScope, Restangular) {
+      var id = $rootScope.$eval('session.username')
+      if (id) {
+        return Restangular.one('groups', id).get()
+      }
+    }
+  }
+
+  return controller
 })
