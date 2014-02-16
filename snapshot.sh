f294e1692ebdbe19d8c8cfecf958f5010ef22b80
Michael Ahlers <michael@ahlers.co>
2014-02-16 01:45:46 -0500

    Allow any host.

--- snapshot.sh
+++ snapshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -H "Content-Type: application/json" --data @"$1" http://localhost:9000/controllers/snapshots
+curl -H "Content-Type: application/json" --data @"$1" http://$2/controllers/snapshots
