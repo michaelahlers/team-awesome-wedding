2740ba3255e1b9bc50e8f3c2dd857ac303b55602
Michael Ahlers <michael@ahlers.co>
2014-02-26 22:15:52 -0500

    Hello World with Google Maps.

--- public/itinerary/index.js
+++ public/itinerary/index.js
@@ -9,13 +9,23 @@ define([
   return angular
     .module('taw.itinerary', [ 'restangular' ])
 
-    .directive('tawItinerary', function () {
+    .directive('tawItinerary', function ($log) {
+      var maps = window.google.maps
+
       return {
         restrict: 'A',
         template: template,
         replace: true,
         scope: true,
         link: function (scope, iEl, Attrs, ctrl) {
+          var map = new maps.Map(iEl.find('.map-canvas')[0], {
+            mapTypeId: maps.MapTypeId.TERRAIN,
+            disableDefaultUI: true,
+            panControl: true,
+            zoomControl: true,
+            zoom: 8,
+            center: new maps.LatLng(-34.397, 150.644)
+          })
         }
       }
     })
