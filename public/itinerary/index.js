266e62edf18a48d49ffa02c6a7512b130450b1f4
Michael Ahlers <michael@ahlers.co>
2014-02-26 22:54:35 -0500

    Start cutting the popovers.

--- public/itinerary/index.js
+++ public/itinerary/index.js
@@ -15,17 +15,20 @@ define([
     {
       name: 'Jaleo',
       address: '2250 Crystal Drive, Arlington, VA 22202',
-      latlng: new maps.LatLng(38.8534779, -77.0495547)
+      latlng: new maps.LatLng(38.8534779, -77.0495547),
+      URL: 'http://jaleo.com/crystal-city'
     },
     {
       name: 'Crystal City Water Park',
-      address: '1700 Crystal Drive, Arlington, VA 22202',
-      latlng: new maps.LatLng(38.8586594, -77.0488019)
+      address: '1750 Crystal Drive, Arlington, VA 22202',
+      latlng: new maps.LatLng(38.8586594, -77.0488019),
+      URL: 'crystalcity.org/active/open-spaces/crystal-city-water-park'
     },
     {
       name: 'Residence Inn',
       address: '2850 Potomac Avenue, Arlington, VA 22202',
-      latlng: new maps.LatLng(38.8479483, -77.051194)
+      latlng: new maps.LatLng(38.8479483, -77.051194),
+      URL: 'http://marriott.com/hotels/travel/wasry-residence-inn-arlington-capital-view'
     }
   ]
 
@@ -50,10 +53,14 @@ define([
           angular.forEach(locations, function (location) {
             bounds.extend(location.latlng)
 
-            new maps.Marker({
+            var marker = new maps.Marker({
               position: location.latlng,
               map: map
             })
+
+            var popup = new maps.InfoWindow()
+            popup.setContent(location.address)
+            popup.open(map, marker)
           })
 
           map.fitBounds(bounds)
